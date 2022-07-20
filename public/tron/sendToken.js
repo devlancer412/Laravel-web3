6671db823941bae229bccb8cf82032d2858b1b38
Joseph Anderson <devanderson0412@gmail.com>
2022-07-20 09:31:32 -0500

    bugfix

--- public/tron/sendToken.js
+++ public/tron/sendToken.js
@@ -3,21 +3,20 @@ let toAddr = process.argv[3];
 let contract = process.argv[4];
 let amount = parseInt(process.argv[5]);
 
-const TronWeb = require('tronweb');
-const TronGrid = require('trongrid');
+const TronWeb = require("tronweb");
+const TronGrid = require("trongrid");
 const HttpProvider = TronWeb.providers.HttpProvider;
-const fullNode = new HttpProvider('https://api.trongrid.io');
-const solidityNode = new HttpProvider('https://api.trongrid.io');
-const eventServer = new HttpProvider('https://api.trongrid.io');
+const fullNode = new HttpProvider("https://api.trongrid.io");
+const solidityNode = new HttpProvider("https://api.trongrid.io");
+const eventServer = new HttpProvider("https://api.trongrid.io");
 var tronWeb = new TronWeb(fullNode, solidityNode, eventServer, pvtKey);
 
 async function sendToken() {
-  // tronWeb.setAddress(contract);
-  const contractIns = await tronWeb.contract().at(contract);
-  await contractIns.transfer(toAddr, amount).send({feeLimit: 10000000});
+    // tronWeb.setAddress(contract);
+    const contractIns = await tronWeb.contract().at(contract);
+    await contractIns.transfer(toAddr, amount).send({ feeLimit: 10000000 });
 }
 
 sendToken()
-  .then(result => console.log(
-    JSON.stringify(result)
-  )).catch(err => console.log(err));
+    .then((result) => console.log(JSON.stringify(result)))
+    .catch((err) => console.log(JSON.stringify(err)));
